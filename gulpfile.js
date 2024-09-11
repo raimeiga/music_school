6f374338082e4c1fc88b37e4a4049c12705a4082
raimeiga <yusuke612@hotmail.com>
2024-09-12 05:04:12 +0900

    調整中

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 // gulpとgulp-sassモジュールを読み込む
 var gulp = require('gulp');
 var sass = require('gulp-sass')(require('sass'));
+var autoprefixer = require('autoprefixer');
+var postcss = require('gulp-postcss');
 
 // Sassのタスクを設定し「{}」の中にSassコンパイルに必要な処理を記述
 gulp.task('sass', function() {
@@ -8,6 +10,7 @@ gulp.task('sass', function() {
   return gulp.src('./scss/**/*.scss')
   //コンパイル時のアウトプットスタイルを「expanded」に指定
   .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+  .pipe(postcss([ autoprefixer() ]))
   // コンパイル先のディレクトリを指定
   .pipe(gulp.dest('./css'));
 });
