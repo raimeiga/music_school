3edcadc4233da50fae37f9dc542a5909463b306c
raimeiga <yusuke612@hotmail.com>
2024-09-23 02:42:42 +0900

    調整中

--- js/main.js
+++ js/main.js
@@ -1,15 +1,62 @@
 $(function() {
 
  // ハンバーガー
- $('.header__hb-btn').click(function(){
+ $('.c-hamburger').click(function(){
    $(this).toggleClass('active')
-   $('.header__nav').toggleClass('active')
+   $('.p-header-nav').toggleClass('active')
  })
 
  $('.header__nav ul li a').click(function(){
-   $('.header__hb-btn').removeClass('active')
-   $('.header__nav').removeClass('active')
+   $('.c-hamburger').removeClass('active')
+   $('.p-header-nav').removeClass('active')
  })
+
+
+ // FVのスライダー
+$('.js-slider').slick({ 
+ autoplay: false, 
+ arrows: true,
+ slidesToShow: 3, // 3枚ずつ表示
+ slidesToScroll: 1, // 1枚ずつスクロール
+ prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-prev.svg" alt="Previous"></button>',  
+ nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-next.svg" alt="Next"></button>',
+ responsive: [
+   {
+     breakpoint: 767, // スマホサイズ
+     settings: {      
+      slidesToShow: 1, // 1枚表示
+      slidesToScroll: 1,     
+      // autoplay,arrows,prevArrow,nextArrowはpc版と重複しているので省略可。
+     }
+   }
+ ]
+});
+
+
+// アコーディオンメニュー
+$('.p-qa-list__q').on('click', function(){
+ $(this).toggleClass('active');
+ $(this).next().slideToggle();
+});
+
+
+// トップに戻るボタンの表示・非表示の切り替え
+// ページトップから、200pxスクロールしたら、トップに
+// 戻るボタンが表示され、200px以下だと非表示に戻る設定
+
+$(window).scroll(function(){
+
+  const conceptOffset = $('.p-fv').offset().top; 
+  const scrollTop = $(this).scrollTop()
+
+  if(scrollTop > 200) { 
+    $('.c-top-back-btn').addClass('active')
+    $('.c-contact').addClass('active')
+  } else {
+    $('.c-top-back-btn').removeClass('active')
+    $('.c-contact').removeClass('active')
+  }
+})
 
 })
 
